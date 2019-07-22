2cb1cb76b2a656a7661ce70290a51d74c0ca4042
billgong(巩志彬) <billgong@wesure.cn>
2019-07-22 14:29:10 +0800

    修改连等赋值，查看赋值引用次数

--- continuous assignment/index.js
+++ continuous assignment/index.js
@@ -1,6 +1,30 @@
-var a = { n: 1 };
+var fooValue = { name: 'foo' };
+var barValue = { name: 'bar' };
+
+Object.defineProperty(global, "foo", {
+    get: function(){
+        console.log("get foo");
+        return fooValue;
+    },
+    set: function(newVal) {
+        console.log("set foo");
+        fooValue = newVal;
+    }
+});
+Object.defineProperty(global, "bar", {
+    get: function(){
+      console.log("get bar");
+      return barValue;
+    },
+    set: function(newVal) {
+        console.log("set bar");
+        barValue = newVal;
+    }
+});
+
+var a = foo;
 var b = a;
-a.x = a = { n: 2 };
+a.x = a = bar;
 // 从右到左赋值，a.x中.等级比=高，仍保留对a的引用
 // https://github.com/Advanced-Frontend/Daily-Interview-Question/issues/93#issuecomment-482389152/
 
