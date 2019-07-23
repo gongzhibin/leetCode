c03d16c466a282b0e88e91c9465787dff39eee1e
gongzhibin <2271721552@qq.com>
2019-07-23 22:05:21 +0800

    code in 2019.7.23

--- 22.generateParenthesis.js
+++ 22.generateParenthesis.js
@@ -3,5 +3,21 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    
+    const dp = [[''], ['()']];
+    for(let i = 2; i <= n; i++) {
+        dp[i] = [];
+        for(let j = 0; j < i; j++) {
+            const left = dp[j]
+            const right = dp[i - 1 - j];
+            for(let k1 = 0; k1 < left.length; k1++) {
+                for(let k2 = 0; k2 < right.length; k2++) {
+                    dp[i].push(`(${left[k1]})${right[k2]}`)
+                }
+            }
+        }
+    }
+    return dp[n];
 };
+
+const res = generateParenthesis(3);
+console.log(res);
